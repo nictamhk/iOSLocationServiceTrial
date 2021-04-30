f8b30f0d40b31498057c20022cced1dc8aa8fe55
nictamhk <sjspfirst@yahoo.com.hk>
2021-04-30 10:15:27 +0800

    Initial Commit

---
+++ LocationService/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LocationService
+//
+//  Created by 譚德朗 on 30/4/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
